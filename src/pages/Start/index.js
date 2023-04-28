8607a4a2aad459304a990319042a5a620c4306cf
baekggum <c3man4@naver.com>
2023-04-28 13:08:34 +0900

    + Start

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -1,7 +1,23 @@
+import Board from "./components/Board";
+import Button from "./components/Button";
+
 const Start = () => {
+  function hello() {
+    console.log("Hello");
+  }
+
   return (
-    <div className="w-screen h-screen bg-blue-500">
-      asd<h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-screen h-screen bg-blue-200 flex justify-center items-center gap-5">
+      <div className="w-full flex justify-center gap-5">
+        <Board />
+        <div className="flex flex-col gap-3 justify-around">
+          <div className="text-5xl font-black">Tic-Tae-Toe</div>
+          <div className="flex gap-3 justify-center">
+            <Button text={"Solo"} color={"bg-rose-200"} onClick={hello} />
+            <Button text={"Multi"} color={"bg-teal-200"} onclick={hello} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
